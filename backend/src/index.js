20d996535b0af4b3dd970967bfbff8a837b0fdfd
Mohammad Rahim bhuiyan <mohammad.rahim.bhuiyan20@gmail.com>
2025-05-29 02:16:38 +0600

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,9 +25,10 @@ app.use(cookieParser())
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
+    origin: "http://31.97.49.55:5173",
+    credentials: true,
 }))
+
 
 // Serve static files from uploads directory
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
